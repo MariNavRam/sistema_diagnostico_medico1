5040550ec102550c4f1720b5026f8bc8776101c4
Eduardo Cabrales <eduardo.cabrales7500@alumnos.udg.mx>
2023-04-16 19:41:45 -0600

    Se añade la columna de acciones a las vistas de index

--- resources/views/app/medico/diagnosticos/index.blade.php
+++ resources/views/app/medico/diagnosticos/index.blade.php
@@ -19,6 +19,7 @@
                         <th class="border border-slate-700 ...">Prueba utilizada</th>
                         <th class="border border-slate-700 ...">Fecha de la cita</th>
                         <th class="border border-slate-700 ...">Descripción del diagnóstico obtenido</th>
+                        <th class="border border-slate-700 ...">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -28,6 +29,10 @@
                         <td class="border border-slate-700 ...">Prueba 1</td>
                         <td class="border border-slate-700 ...">Cita 1</td>
                         <td class="border border-slate-700 ...">Descripción 1</td>
+                        <td>
+                            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-1 py-0.5 text-center mr-2 mb-2 ">Editar</button>
+                            <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-2 py-0.5 text-center mr-2 mb-2">Eliminar</button>
+                        </td>
                         </tr>
                         <tr>
                         <td class="border border-slate-700 ...">Paciente 2</td>
@@ -35,6 +40,10 @@
                         <td class="border border-slate-700 ...">Prueba 2</td>
                         <td class="border border-slate-700 ...">Cita 2</td>
                         <td class="border border-slate-700 ...">Descripción 2</td>
+                        <td>
+                            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-1 py-0.5 text-center mr-2 mb-2 ">Editar</button>
+                            <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-2 py-0.5 text-center mr-2 mb-2">Eliminar</button>
+                        </td>
                         </tr>
                         <tr>
                         <td class="border border-slate-700 ...">Paciente 3</td>
@@ -42,6 +51,10 @@
                         <td class="border border-slate-700 ...">Prueba 3</td>
                         <td class="border border-slate-700 ...">Cita 3</td>
                         <td class="border border-slate-700 ...">Descripción 3</td>
+                        <td>
+                            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-1 py-0.5 text-center mr-2 mb-2 ">Editar</button>
+                            <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-2 py-0.5 text-center mr-2 mb-2">Eliminar</button>
+                        </td>
                         </tr>
                     </tbody>
                 </table>
